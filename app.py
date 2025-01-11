f7ba490fb7cc251fce001e4eef8363a43406f9af
Jobin P John <pjobin260@gmail.com>
2025-01-12 02:59:32 +0530

    New app script added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/search', methods=['POST'])
+def search():
+    from_location = request.form['from']
+    to_location = request.form['to']
+    departure_date = request.form['departure']
+    results = f"Searching flights from {from_location} to {to_location} on {departure_date}"
+    return render_template('index.html', results=results)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
+
